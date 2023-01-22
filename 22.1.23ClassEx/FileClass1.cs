79eb36563368aeda4f7ff847f95c44d1332a1ca8
Hay_Abadi <abados9891@gmail.com>
2023-01-22 12:26:16 +0200

    CheckSizeFunc

--- 22.1.23ClassEx/FileClass1.cs
+++ 22.1.23ClassEx/FileClass1.cs
@@ -8,21 +8,21 @@ using System.Threading.Tasks;
 namespace _22._1._23ClassEx
 {
     public class FileClass1
-    {
+    { // private variables to store file name, path, base name and serial number
         private string fileName;
         private string path;
         private string baseName;
         private string newFileName;
         private int fileNumber = 1;
 
-        public FileClass1(string fileName)
+        public FileClass1(string fileName) // constructor to accept file name as parameter
         {
-            this.fileName = fileName;
-            this.path = Path.GetDirectoryName(fileName);
-            this.baseName = Path.GetFileNameWithoutExtension(fileName);
+            this.fileName = fileName;   
+            this.path = Path.GetDirectoryName(fileName);// get the directory path of the file
+            this.baseName = Path.GetFileNameWithoutExtension(fileName); //get the base name of the file (without extension)
         }
 
-        public void CreateFile()
+        public void CreateFile() // function to create the file if it doesn't exist
         {
             if (!File.Exists(fileName))
             {
@@ -37,40 +37,48 @@ namespace _22._1._23ClassEx
             }
         }
 
-        public void WriteToFile(string text)
+        public void WriteToFile(string text)  // function to write text to the file
         {
-            FileInfo file = new FileInfo(fileName);
+            FileInfo file = new FileInfo(fileName);   // create a FileInfo object to check the size of the file
 
-            if (file.Length >= 1000)
+            CheckSize(file);
+
+            using (StreamWriter sw = File.AppendText(fileName))
             {
-                newFileName = path + "\\" + baseName + fileNumber + ".txt";
+                sw.WriteLine(text);
+            }
+        }
+
+        public void CheckSize(FileInfo file)  // function to write text to the file
+        {
+
+            if (file.Length >= 1000)   // check if the file size is greater than or equal to 1000kb
+            {
+                newFileName = path + "\\" + baseName + fileNumber + ".txt"; // create new file name
                 fileNumber++;
-                if (!File.Exists(newFileName))
+                if (!File.Exists(newFileName)) // check if the new file name already exists
                 {
-                    using (FileStream fs = File.Create(newFileName))
+                    using (FileStream fs = File.Create(newFileName)) // create new file
                     {
                         Console.WriteLine("New file created successfully.");
                     }
                 }
-                else
+                else // if the new file name already exists, increment the serial number and check again
                 {
                     while (File.Exists(newFileName))
                     {
                         fileNumber++;
                         newFileName = path + "\\" + baseName + fileNumber + ".txt";
                     }
-                    using (FileStream fs = File.Create(newFileName))
+                    using (FileStream fs = File.Create(newFileName))    // create new file
                     {
                         Console.WriteLine("New file created successfully.");
                     }
                 }
-                fileName = newFileName;
+                fileName = newFileName; // update the file name to use the new file name
+
             }
 
-            using (StreamWriter sw = File.AppendText(fileName))
-            {
-                sw.WriteLine(text);
-            }
         }
     }
 }
